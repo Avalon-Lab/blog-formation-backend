fb69c98a2d19195b2b28f945720e2d35c65b7233
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2020-06-17 13:28:56 +0200

    Test avec l'annotAtion @CaptureTransaction de l'agent APM

--- src/main/kotlin/fr/avalonlab/blog/workshop/blogpost/BlogPostController.kt
+++ src/main/kotlin/fr/avalonlab/blog/workshop/blogpost/BlogPostController.kt
@@ -1,5 +1,6 @@
 package fr.avalonlab.blog.workshop.blogpost
 
+import co.elastic.apm.api.CaptureTransaction
 import io.jooby.annotations.*
 import javax.inject.Inject
 import org.apache.logging.log4j.kotlin.logger
@@ -10,12 +11,14 @@ import java.time.ZoneOffset
 class BlogPostController @Inject constructor(private val blogPostRepository: BlogPostRepository) {
 
     @GET("/")
+    @CaptureTransaction
     fun getAllBlogPosts() : List<BlogPost> {
         logger().info { "Get all BlogPost" }
         return blogPostRepository.findAll()
     }
 
     @POST("/")
+    @CaptureTransaction
     fun addBlogPost(blogPost: BlogPost) {
         logger().info { "Add a new BlogPost" }
         blogPost.creationDate = OffsetDateTime.now(ZoneOffset.UTC).toString()
@@ -23,6 +26,7 @@ class BlogPostController @Inject constructor(private val blogPostRepository: Blo
     }
 
     @PUT("/")
+    @CaptureTransaction
     fun updateBlogPost(blogPost: BlogPost) {
         logger().info { "Update a BlogPost" }
         blogPost.updateDate = OffsetDateTime.now(ZoneOffset.UTC).toString()
@@ -30,24 +34,28 @@ class BlogPostController @Inject constructor(private val blogPostRepository: Blo
     }
 
     @DELETE("/")
+    @CaptureTransaction
     fun deleteAllBlogPosts() {
         logger().info { "Delete all BlogPosts" }
         blogPostRepository.removeAll()
     }
 
     @GET("/{id}")
+    @CaptureTransaction
     fun getBlogPost(@PathParam id: String) : BlogPost {
         logger().info { "Retrieve BlogPost $id" }
         return blogPostRepository.findById(id)!!
     }
 
     @DELETE("/{id}")
+    @CaptureTransaction
     fun deleteBlogPost(@PathParam id: String) {
         logger().info { "Delete BlogPost $id" }
         blogPostRepository.removeById(id)
     }
 
     @GET("/author/{name}")
+    @CaptureTransaction
     fun getBlogPostFromAuthor(@PathParam name: String): List<BlogPost> {
         logger().info { "Retrieve $name's (author) BlogPosts" }
         return blogPostRepository.find("author",name)
