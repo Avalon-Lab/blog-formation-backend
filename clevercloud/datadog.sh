d8ff0c25e3dc38cd96b6b4015640c7faca097f4c
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2020-07-01 11:00:27 +0200

    Make dd agent executable

--- clevercloud/datadog.sh
+++ clevercloud/datadog.sh
@@ -4,6 +4,8 @@ echo "Getting Datadog Agent"
 
 wget --quiet 'https://avalon-lab.gitlab.io/oss/datadog-agent-build/agent' -P ${APP_HOME}/datadog
 
+chmod +x ${APP_HOME}/datadog/agent
+
 echo "Run Datadog Agent"
 
 ${APP_HOME}/datadog/agent run -c ${APP_HOME}/datadog/datadog.yaml
